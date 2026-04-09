7e3793000cde1950a9187cf5a5cecab2aa0d6c83
Bi <rappergb@gmail.com>
2026-04-09 15:54:27 +0700

    add api get all dich vu

--- SCI_SE_SourceCode_BackEnd_G6_052026/app/Http/Controllers/DichVuController.php
+++ SCI_SE_SourceCode_BackEnd_G6_052026/app/Http/Controllers/DichVuController.php
@@ -7,21 +7,28 @@ use Illuminate\Http\Request;
 
 class DichVuController extends Controller
 {
+    public function getDichVu()
+    {
+        $dichVu = DichVu::get();
+        return response()->json([
+            'status' => true,
+            'data' => $dichVu
+        ]);
+    }
     public function chiTietDichVu($id)
     {
         $dichVu = DichVu::where('id', $id)
-                        
-                        ->select('dich_vus.id', 'dich_vus.ten_dich_vu', 'dich_vus.mo_ta_ngan', 'dich_vus.mo_ta_chi_tiet', 'dich_vus.gia_tien', 'dich_vus.hinh_anh')
-                        ->first();
+            ->select('dich_vus.id', 'dich_vus.ten_dich_vu', 'dich_vus.mo_ta_ngan', 'dich_vus.mo_ta_chi_tiet', 'dich_vus.gia_tien', 'dich_vus.hinh_anh')
+            ->first();
         if (!$dichVu) {
             return response()->json([
-                'success' => false,
+                'status' => false,
                 'message' => 'Không tìm thấy dịch vụ với ID đã cho.'
             ], 404);
         }
 
         return response()->json([
-            'success' => true,
+            'status' => true,
             'data' => $dichVu
         ]);
     }
@@ -30,13 +37,13 @@ class DichVuController extends Controller
         $data = DichVu::where('ten_dich_vu', 'LIKE', '%' . $keyword . '%')->get();
         if ($data->isEmpty()) {
             return response()->json([
-                'success' => false,
+                'status' => false,
                 'message' => 'Không tìm thấy dịch vụ nào phù hợp với từ khóa.'
             ], 404);
         } else {
 
             return response()->json([
-                'success' => true,
+                'status' => true,
                 'data' => $data
             ]);
         }
